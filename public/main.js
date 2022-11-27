c840713ce9a0349db2c9294dab3e891ef409681f
Daniel Fleuri <hiru.13@hotmail.com>
2022-11-27 19:29:17 -0300

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,15 @@
+
+function retrieveRepos() {
+    const user = 'danfleuri13'
+    const container = document.getElementById("main");
+
+}
+
+function showHome() {
+    document.getElementById("main").style.display = 'grid';
+    document.getElementById("profile").style.display = 'none';
+}
+function showProfile() {
+    document.getElementById("main").style.display = 'none';
+    document.getElementById("profile").style.display = 'grid';
+}
