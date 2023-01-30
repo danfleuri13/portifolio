ee99ef62e06c013ffb0c9bfe4899d3f87d409d6c
Daniel Fleuri <hiru.13@hotmail.com>
2023-01-29 21:00:28 -0300

    atualizacao

--- public/main.js
+++ public/main.js
@@ -1,17 +1,23 @@
-
 function retrieveRepos() {
-    const user = 'danfleuri13'
+    const user = 'danfleuri13';
     const container = document.getElementById("main");
-
 }
 
 function showHome() {
     document.getElementById("main").style.display = 'grid';
     document.getElementById("profile").style.display = 'none';
+    document.getElementById("exp").style.display = 'none';
 }
 function showProfile() {
     document.getElementById("main").style.display = 'none';
     document.getElementById("profile").style.display = 'grid';
+    document.getElementById("exp").style.display = 'none';
+}
+
+function showExp() {
+    document.getElementById("main").style.display = 'none';
+    document.getElementById("profile").style.display = 'none';
+    document.getElementById("exp").style.display = 'grid';
 }
 
 
